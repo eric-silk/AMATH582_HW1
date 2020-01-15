a9383cb84e51a29ed0b0c7c97d83fc0a28e96ca1
Eric Silk <eric.silk@ericsilk.com>
2020-01-14 17:37:31 -1000

    Trying to understand the data handed in.

--- example_code.m
+++ example_code.m
@@ -1,16 +1,44 @@
 clear all; close all; clc;
 load Testdata
+SAMPLES = 20;
 L=15; % spatial domain
 n=64; % Fourier modes
 x2=linspace(-L,L,n+1); x=x2(1:n); y=x; z=x;
 k=(2*pi/(2*L))*[0:(n/2-1) -n/2:-1]; ks=fftshift(k);
 [X,Y,Z]=meshgrid(x,y,z);
 [Kx,Ky,Kz]=meshgrid(ks,ks,ks);
-%{ This seems to break octave lol
-for j=1:20
-  Un(:,:,:)=reshape(Undata(j,:),n,n,n);
-  close all, isosurface(X,Y,Z,abs(Un),0.4)
-  axis([-20 20 -20 20 -20 20]), grid on, drawnow
-  pause(1)
+
+
+if false
+figure();
+for j=1:SAMPLES
+    Un(:,:,:)=reshape(Undata(j,:),n,n,n);
+    isosurface(X,Y,Z,abs(Un),0.5)
+    axis([-20 20 -20 20 -20 20]), grid on
+    %winput("Press anything to continue...");
 end
-%}
+drawnow;
+end
+
+
+
+% Plot the averaged spectrums
+averaged_spectrum = zeros(n, n, n);
+figure();
+for j=1:SAMPLES
+    tmp(:,:,:) = reshape(Undata(j, :), n, n, n);
+    fft_tmp = fftn(tmp);
+    subplot(4,5,j);
+    histogram(abs(fft_tmp))
+    averaged_spectrum = averaged_spectrum + tmp;
+end
+
+
+
+averaged_spectrum = averaged_spectrum / SAMPLES;
+figure()
+histogram(abs(averaged_spectrum))
+input("Enter anything to continue...")
+close all
+isosurface(X, Y, Z, abs(fftshift(averaged_spectrum)), 0.2)
+axis([-20 20 -20 20 -20 20]); grid on, drawnow
---
+++ fft_exploration.m
@@ -0,0 +1,27 @@
+close all; clear all; clc
+n = 256;
+L = 1;
+
+t = linspace(-L, L, n);
+freq = 8;
+y = sin(2*pi*freq*t);
+
+figure();
+subplot(311);
+plot(t,y);
+
+k = (1/(2*L))*[0:(n/2-1) -n/2:-1];
+ks = fftshift(k);
+Y = fftshift(fft(y));
+subplot(312);
+stem(ks, abs(Y))
+
+yt = ifft(fftshift(Y));
+subplot(313)
+plot(t, yt)
+
+figure();
+hist(abs(Y))
+
+% Now lets try 2D
+t2x= meshgrid(t, t);
